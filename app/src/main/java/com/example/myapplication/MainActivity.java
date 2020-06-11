3cec081131e2f959b4425ab764534a887c40569b
nikolay.vlaskin <nikolay.vlaskin@gmail.com>
2020-06-12 03:01:07 +0700

    +

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.text);
+
+
+        String testName = getResources().getString(R.string.test_name);
+    }
+}
